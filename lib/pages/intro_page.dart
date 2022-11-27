faa91fea5795d8fe913ddd6f52034b1c80845b9b
Beyza Evcen <beyzaevcen37@icloud.com>
2022-11-27 17:50:50 +0300

    new

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget{
+  const IntroPage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
