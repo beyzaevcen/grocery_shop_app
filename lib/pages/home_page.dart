ccad59b099d6dcb4e8d303345ca114fda9db1102
Beyza Evcen <beyzaevcen37@icloud.com>
2022-11-28 23:41:37 +0300

    new

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../components/grocery_item_tile.dart';
+
 class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
@@ -10,25 +12,50 @@ class HomePage extends StatelessWidget{
           crossAxisAlignment: CrossAxisAlignment.start,
           children:  [
             const SizedBox(height: 50.0,),
-const Padding(
-  padding: const EdgeInsets.symmetric(horizontal: 24.0),
-  child:   Text("Good morning babe<3"),
-),
+            const Padding(
+             padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child:   Text("Good morning babe<3"),
+            ),
             const SizedBox(height: 4.0,),
-            //lets order fresh items for you
- Padding(
-  padding: const EdgeInsets.symmetric(horizontal: 24.0),
-  child:   Text(
-      "Let's order fresh items for you!",
-  style: GoogleFonts.notoSerif(
-    fontSize: 36.0,
-    fontWeight: FontWeight.bold,
-  ),
-  ),
-),
-            //divider
 
-            //fresh items+grid
+
+            Padding(
+             padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child:   Text(
+                "Let's order fresh items for you!",
+                style: GoogleFonts.notoSerif(
+                        fontSize: 36.0,
+                  fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+            const SizedBox(height: 24.0,),
+
+             const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: Divider(),
+            ),
+            const SizedBox(height:24.0,),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "Fresh Items!",
+                 style: TextStyle(
+                 fontSize: 16
+              ),
+              ),
+            ),
+
+
+
+            Expanded(
+                child:GridView.builder(gridDelegate:
+                const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                    itemBuilder: (context,index){
+                  return GroceryItemTile();
+                    },
+                    ),
+            ),
           ],
 
         ),
